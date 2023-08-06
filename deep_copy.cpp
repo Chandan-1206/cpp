a7846806e234c8525bb1d96d5fdad594a4aea285
Chandan-1206 <137202098+Chandan-1206@users.noreply.github.com>
2023-08-06 23:43:03 +0530

    Create deep_copy.cpp

---
+++ deep_copy.cpp
@@ -0,0 +1,37 @@
+//deep copy
+#include <iostream>
+
+using namespace std;
+
+class demo{
+    int data1,data2,*p;
+    public:
+    demo(){
+        p=new int;
+    }
+    void getdata(int a,int b,int c){
+        data1=a;
+        data2=b;
+        *p=c;
+    }
+    demo(demo &obj){
+        data1 =obj.data1;
+        data2 =obj.data2;
+        p=new int; //this makes a copy of variable pointer pointing to (main resaon for deep copy).
+        p=(obj.p);
+    }
+    void display(){
+        cout<<"data1 = "<<data1<<"\tdata2 = "<<data2<<"\t*p = "<<*p<<endl;
+    }
+};
+
+int main()
+{
+    demo obj1;
+    obj1.getdata(11,33,55);
+    demo obj2=obj1;
+    obj1.display();
+    obj2.display();
+
+    return 0;
+}
