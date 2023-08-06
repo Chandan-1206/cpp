6129f5ed7018c07039fb965e5fc4f40f80ab169d
Chandan-1206 <137202098+Chandan-1206@users.noreply.github.com>
2023-08-06 23:41:42 +0530

    Create Shallowcopy_copyconstructor.cpp

---
+++ Shallowcopy_copyconstructor.cpp
@@ -0,0 +1,31 @@
+//shallow copy( copy constructor)
+#include <iostream>
+
+using namespace std;
+
+class demo{
+    int data1,data2,*p;
+    public:
+    demo(){
+        p=new int;
+    }
+    void getdata(int a,int b,int c){
+        data1=a;
+        data2=b;
+        *p=c;
+    }
+    void display(){
+        cout<<"data1 = "<<data1<<"\tdata2 = "<<data2<<"\t*p = "<<*p<<endl;
+    }
+};
+
+int main()
+{
+    demo obj1;
+    obj1.getdata(11,33,55);
+    //here copy constructutor is working 
+    demo obj2=obj1;
+    obj1.display();
+    obj2.display();
+
+    return 0;
