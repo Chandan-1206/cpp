240ebbd3ffcf7b9bf6962467b022776e4be9e9f0
Chandan-1206 <137202098+Chandan-1206@users.noreply.github.com>
2023-08-17 22:57:50 +0530

    Create single_cll_insertion.cpp

---
+++ single_cll_insertion.cpp
@@ -0,0 +1,50 @@
+// circular linked list-insertionat end by rear pointer
+#include <iostream>
+
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node *next;
+    node(int x){
+        data=x;
+        next=NULL;
+    }
+};
+node *rear=NULL;
+
+void insertend(int x){
+    node *temp=new node(x);
+    if(rear==NULL){
+        temp->next=temp;
+    }
+    else{
+        temp->next=rear->next;
+        rear->next=temp;
+    }
+    rear=temp;
+}
+
+void print(){
+    node *temp=rear->next;
+    while(temp!=rear){
+        cout<<temp->data<<"\t";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+
+
+int main()
+{
+    int x;
+    for(int i=0;i<6;i++){
+        x=rand()%10;
+        insertend(x);
+    }
+    print();
+
+
+    return 0;
+}
